ed8415d63a15cea9f1a4b886bd3a263f2854de23
Rodrigo Martins <rodrigo.sousa.577@gmail.com>
2025-03-08 00:30:37 -0300

    commit

--- accel-pppd/include/ap_session.h
+++ accel-pppd/include/ap_session.h
@@ -3,9 +3,14 @@
 
 #include <stdint.h>
 #include <sys/socket.h>
+#include <time.h>
+#include <linux/rtnetlink.h>
 
 #include "list.h"
 #include "triton.h"
+
+/* Define atomic_t */
+typedef struct { volatile int counter; } atomic_t;
 
 /* AP_STATE flags */
 #define AP_STATE_STARTING     1
@@ -37,11 +42,16 @@
 #define TERM_IPCPv6_DISABLED           16
 #define TERM_KEEPALIVE_TIMEOUT         17
 #define TERM_PACKET_TIMEOUT            18
-#define TERM_SERVICE_UNAVAILABLE       19
+#define TERM_SERVICE_UNAVAILABLE        19
+#define TERM_NAS_ERROR                 20
 
 struct ap_session;
 struct backup_data;
 struct rtnl_link_stats;
+struct rtnl_link_stats64;
+struct ipv4db_item_t;
+struct ipv6db_item_t;
+struct net;
 
 struct ap_ctrl {
     struct triton_context_t *ctx;
@@ -74,6 +84,9 @@ struct ap_session {
     
     int state;
     int terminating:1;
+    int terminated:1;
+    int down:1;
+    int unit_idx;
     
     struct triton_timer_t timer;
     
@@ -90,6 +103,16 @@ struct ap_session {
     uint64_t tx_bytes;
     uint64_t rx_packets;
     uint64_t tx_packets;
+
+    /* Contadores para estatísticas de accounting */
+    uint64_t acct_rx_bytes;
+    uint64_t acct_tx_bytes;
+    uint64_t acct_rx_packets;
+    uint64_t acct_tx_packets;
+    uint64_t acct_rx_bytes_i;
+    uint64_t acct_tx_bytes_i;
+    uint64_t acct_rx_packets_i;
+    uint64_t acct_tx_packets_i;
     
     int terminate_cause;
     
@@ -103,9 +126,35 @@ struct ap_session {
     char *username;
     char *sessionid;
     char *comp;
-        
+    
+    time_t start_time;
+    time_t idle_time;
+    time_t stop_time;
+    
+    int idle_timeout;
+    int session_timeout;
+    
+    /* Contexto para despertar a thread */
+    struct triton_context_t *wakeup;
+    
+    /* Recursos de rede */
+    struct net *net;
+    struct ipv4db_item_t *ipv4;
+    struct ipv6db_item_t *ipv6;
+    
+    /* Nome da VRF */
+    char *vrf_name;
+    
     struct list_head pd_list;
 };
+
+struct ap_session_stat {
+    int starting;
+    int active;
+    int finishing;
+};
+
+extern struct ap_session_stat ap_session_stat;
 
 void ap_session_init(struct ap_session*);
 void ap_session_set_ifname(struct ap_session*, const char *ifname);
@@ -114,14 +163,17 @@ int ap_session_started(struct ap_session*);
 void ap_session_finished(struct ap_session*);
 void ap_session_terminate(struct ap_session*, int cause, int hard);
 void ap_session_free(struct ap_session*);
-void ap_session_read_stats(struct ap_session*);
-int ap_session_rename(struct ap_session *, const char *ifname, int len);
+int ap_session_read_stats(struct ap_session*, struct rtnl_link_stats64 *stats);
+int ap_session_rename(struct ap_session*, const char *ifname, int len);
 int ap_session_ifcfg(struct ap_session*);
 void ap_session_accounting_started(struct ap_session*);
 int ap_session_set_username(struct ap_session*, const char *username);
+int ap_session_vrf(struct ap_session*, const char *vrf_name, int len);
+void ap_session_ifup(struct ap_session*);
+void ap_session_ifdown(struct ap_session*);
 
 void ap_session_mppe_keys(struct ap_session *ses, uint8_t *send_key, int send_key_len, uint8_t *recv_key, int recv_key_len);
 
-void ap_shutdown_soft(void);
+int ap_shutdown_soft(void (*cb)(void), int term);
 
 #endif
